35450e6aadc0bff872dff7fd724bc02c8263ff6b
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-10 18:06:47 +0200

    nombre de los empleados

--- EjercicioA/codigo.py
+++ EjercicioA/codigo.py
@@ -1,5 +1,9 @@
 class empleados :
     def __init__(self, nombre):
         self.nombre = nombre
+empleado1 = empleados("Sr. Martin")
+empleado2 = empleados("Sr. salim")
+empleado3 = empleados("Sra. Xing")
+
 
         
