c80529953984772ead03f51741273d85ec0aabbc
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-04-10 18:11:16 +0200

    empresa

--- EjercicioA/codigo.py
+++ EjercicioA/codigo.py
@@ -5,15 +5,17 @@ empleado1 = empleados("Sr. Martin")
 empleado2 = empleados("Sr. salim")
 empleado3 = empleados("Sra. Xing")
 
-class empresa :
-    def __init__(self, nombre):
-        self.nombre = nombre
-    empleados = []
+
 class edificio :
     def __init__(self, letra):
         self.letra = letra
     empleados = []
-    empresas = []
+
+class empresa :
+    def __init__(self, nombre):
+        self.nombre = nombre
+    empleados = []
+    edificios = []
 
 
 
